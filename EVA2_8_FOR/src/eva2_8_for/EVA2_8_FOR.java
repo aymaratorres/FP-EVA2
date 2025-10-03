e179ab9ee89a1571d876643c920814f1d928944e
aymaratorres <L25550860@chihuahua2.tecnm.mx>
2025-10-03 16:52:20 -0600

    Add files via upload

---
+++ EVA2_8_FOR/src/eva2_8_for/EVA2_8_FOR.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_8_for;
+
+/**
+ *
+ * @author Aymara
+ */
+public class EVA2_8_FOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       //"FOR" necesita 3 parámetros (valores) para iniciar
+       //Variable y valor de inicio
+       //Hasta donde llegas es una "CONDICIÓN" --> Tiene que ser verdad y cuando es Falso el "FOR" se detiene
+       //Incremento o decremento (Para controlar esto, se SUMA O RESTA, OPERADORES: AÑADIR 1 O RESTAR 1, ++ (Se suma a la "i") --
+        //for(int i = 1; i <= 10; i++ ){
+        //System.out.println("HOLA MUNDO!!");
+        //}
+        
+        for(int i = 10; i >= 1; i--){
+        System.out.println(i);
+        }
+    }
+    
+}
