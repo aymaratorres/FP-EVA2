e5715dd807acbfe13871bbbd8a77d741e4074332
aymaratorres <L25550860@chihuahua2.tecnm.mx>
2025-11-11 15:42:20 -0600

    Add files via upload

---
+++ EVA2_20_CICLO_INFINITO/src/eva2_20_ciclo_infinito/EVA2_20_CICLO_INFINITO.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_20_ciclo_infinito;
+
+/**
+ *
+ * @author Aymara
+ */
+public class EVA2_20_CICLO_INFINITO {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args){
+        while(true){
+        System.out.println("Aymara Torres");
+        }
+    }
+}
+// "break" ----> Efecto de terminar el ciclo.
+// "continue" ----> Terminar la repetición (interacción).
