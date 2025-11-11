98b09b5eb345b2ef2967f45a45ee59506cd3dd89
aymaratorres <L25550860@chihuahua2.tecnm.mx>
2025-11-11 15:46:51 -0600

    Add files via upload

---
+++ EVA2_21_BREAK/src/eva2_21_break/EVA2_21_BREAK.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_21_break;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Aymara
+ */
+public class EVA2_21_BREAK {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args){
+        Scanner captu = new Scanner(System.in);
+        int valor, adivinar;
+        adivinar = (int)(Math.random() * 5) + 1;// números entre 1 y 5.
+        while(true){
+            System.out.println("Adivina el número. -1 para terminar.");
+            valor = captu.nextInt();
+            if(valor == adivinar){
+            System.out.println("Felicidades, adivinaste!!");
+            break;
+            }
+            if(valor == -1)
+            break;// Termina el ciclo.
+        }
+        System.out.println("Gracias por jugar!!");
+    }
+}
